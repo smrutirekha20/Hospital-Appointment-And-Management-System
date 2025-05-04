d47ca19414f5b1619f6a2a461e9ae480d65cd072
smrutirekha20 <dassmrutirekha20@gmail.com>
2025-05-04 19:23:11 +0530

    update appointment request and response dtos and enhance appointment mapper for data transformation

--- src/main/java/com/hams/hamsvc/mapper/AppointmentMapper.java
+++ src/main/java/com/hams/hamsvc/mapper/AppointmentMapper.java
@@ -3,20 +3,25 @@ package com.hams.hamsvc.mapper;
 import com.hams.hamsvc.entity.Appointment;
 import com.hams.hamsvc.requestDTO.AppointmentRequest;
 import com.hams.hamsvc.responseDTO.AppointmentResponse;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AppointmentMapper {
 
-    public Appointment mapToAppointment(AppointmentRequest appointmentRequest){
-        Appointment appointment=new Appointment();
-        appointment.setAppointmentDateTime(appointmentRequest.getAppointmentDateTime());
+    public Appointment mapToAppointment(AppointmentRequest appointmentRequest) {
+        Appointment appointment = new Appointment();
         appointment.setAppointmentStatus(appointmentRequest.getAppointmentStatus());
         return appointment;
     }
-    public AppointmentResponse mapToAppointmentResponse(Appointment appointment){
-        AppointmentResponse appointmentResponse=new AppointmentResponse();
+
+    public AppointmentResponse mapToAppointmentResponse(Appointment appointment) {
+        AppointmentResponse appointmentResponse = new AppointmentResponse();
         appointmentResponse.setAppointmentId(appointment.getAppointmentId());
-        appointmentResponse.setAppointmentDateTime(appointment.getAppointmentDateTime());
+        appointmentResponse.setAppointmentDateAndTime(appointment.getAppointmentDateAndTime());
         appointmentResponse.setAppointmentStatus(appointment.getAppointmentStatus());
+        appointmentResponse.setDoctorName(appointment.getDoctor().getName());
+        appointmentResponse.setSlotDate(appointment.getSlot().getSlotDate());
+        appointmentResponse.setSlotTime(appointment.getSlot().getSlotTime());
 
         return appointmentResponse;
     }
--- src/main/java/com/hams/hamsvc/responseDTO/AppointmentResponse.java
+++ src/main/java/com/hams/hamsvc/responseDTO/AppointmentResponse.java
@@ -2,31 +2,65 @@ package com.hams.hamsvc.responseDTO;
 
 import com.hams.hamsvc.enums.AppointmentStatus;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class AppointmentResponse {
 
     private Integer appointmentId;
     private LocalDateTime appointmentDateAndTime;
     private AppointmentStatus appointmentStatus;
+    private String doctorName;
+    private LocalDate slotDate;
+    private LocalTime slotTime;
+
+    public Integer getAppointmentId() {
+        return appointmentId;
+    }
 
     public void setAppointmentId(Integer appointmentId) {
         this.appointmentId = appointmentId;
     }
 
-    public Integer getAppointmentId(){
-        return appointmentId;
-    }
-    public LocalDateTime getAppointmentDateTime() {
+    public LocalDateTime getAppointmentDateAndTime() {
         return appointmentDateAndTime;
     }
-    public void setAppointmentDateTime(LocalDateTime appointmentDateTime) {
-        this.appointmentDateAndTime = appointmentDateTime;
+
+    public void setAppointmentDateAndTime(LocalDateTime appointmentDateAndTime) {
+        this.appointmentDateAndTime = appointmentDateAndTime;
     }
+
     public AppointmentStatus getAppointmentStatus() {
         return appointmentStatus;
     }
+
     public void setAppointmentStatus(AppointmentStatus appointmentStatus) {
         this.appointmentStatus = appointmentStatus;
     }
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+
+    public void setDoctorName(String doctorName) {
+        this.doctorName = doctorName;
+    }
+
+    public LocalDate getSlotDate() {
+        return slotDate;
+    }
+
+    public void setSlotDate(LocalDate slotDate) {
+        this.slotDate = slotDate;
+    }
+
+    public LocalTime getSlotTime() {
+        return slotTime;
+    }
+
+    public void setSlotTime(LocalTime slotTime) {
+        this.slotTime = slotTime;
+    }
+
 }
