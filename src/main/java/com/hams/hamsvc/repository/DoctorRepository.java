fd0e70e8a5342e8d2b33f6f197653ff2b1e5ea75
smrutirekha20 <dassmrutirekha20@gmail.com>
2025-05-04 19:27:49 +0530

    aadd findByName method to doctorRepository for fetching doctor by name

--- src/main/java/com/hams/hamsvc/repository/DoctorRepository.java
+++ src/main/java/com/hams/hamsvc/repository/DoctorRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor,Integer> {
 
     boolean existsByEmail(String email);
     List<Doctor> findByDepartmentAndSpecialization(Department department, Specialization specialization);
+    Optional<Doctor> findByName(String name);
 }
