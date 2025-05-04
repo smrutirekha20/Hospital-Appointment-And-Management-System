6bde944a471d8bbd21ca4e9ec06a469d3ceb92e0
smrutirekha20 <dassmrutirekha20@gmail.com>
2025-05-04 19:20:33 +0530

    creating Appointment entity with new fields and relationships

--- src/main/java/com/hams/hamsvc/entity/Appointment.java
+++ src/main/java/com/hams/hamsvc/entity/Appointment.java
@@ -10,50 +10,75 @@ import java.time.LocalDateTime;
 public class Appointment {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "appointment_id")
     private Integer appointmentId;
 
     @Column(name = "appointment_date_and_time")
     private LocalDateTime appointmentDateAndTime;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "appointment_status")
     private AppointmentStatus appointmentStatus;
 
     @ManyToOne
+    @JoinColumn(name = "patient_id")
     private Patient patient;
 
     @ManyToOne
+    @JoinColumn(name = "doctor_id")
     private Doctor doctor;
+
+    @OneToOne
+    @JoinColumn(name = "slot_id")
+    private Slot slot;
 
     public Integer getAppointmentId() {
         return appointmentId;
     }
+
     public void setAppointmentId(Integer appointmentId) {
         this.appointmentId = appointmentId;
     }
-    public LocalDateTime getAppointmentDateTime() {
+
+    public LocalDateTime getAppointmentDateAndTime() {
         return appointmentDateAndTime;
     }
-    public void setAppointmentDateTime(LocalDateTime appointmentDateTime) {
-        this.appointmentDateAndTime = appointmentDateTime;
+
+    public void setAppointmentDateAndTime(LocalDateTime appointmentDateAndTime) {
+        this.appointmentDateAndTime = appointmentDateAndTime;
     }
+
     public AppointmentStatus getAppointmentStatus() {
         return appointmentStatus;
     }
+
     public void setAppointmentStatus(AppointmentStatus appointmentStatus) {
         this.appointmentStatus = appointmentStatus;
     }
+
     public Patient getPatient() {
         return patient;
     }
+
     public void setPatient(Patient patient) {
         this.patient = patient;
     }
+
     public Doctor getDoctor() {
         return doctor;
     }
+
     public void setDoctor(Doctor doctor) {
         this.doctor = doctor;
+    }
+
+    public Slot getSlot() {
+        return slot;
+    }
+
+    public void setSlot(Slot slot) {
+        this.slot = slot;
     }
 
 }
