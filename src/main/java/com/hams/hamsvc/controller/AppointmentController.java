d2a605a0ac99818955bd298a76d9ed8701a8d236
smrutirekha20 <dassmrutirekha20@gmail.com>
2025-05-04 19:36:54 +0530

    adding appointmentcontroller to handle appointment related like book appointments requests

--- src/main/java/com/hams/hamsvc/controller/AppointmentController.java
+++ src/main/java/com/hams/hamsvc/controller/AppointmentController.java
@@ -1,5 +1,7 @@
 package com.hams.hamsvc.controller;
 
+import com.hams.hamsvc.requestDTO.AppointmentRequest;
+import com.hams.hamsvc.responseDTO.AppointmentResponse;
 import com.hams.hamsvc.responseDTO.DoctorResponse;
 import com.hams.hamsvc.service.AppointmentService;
 import com.hams.hamsvc.utility.AppResponseBuilder;
@@ -7,10 +9,7 @@ import com.hams.hamsvc.utility.ResponseStructure;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +25,10 @@ public class AppointmentController {
     public ResponseEntity<ResponseStructure<List<DoctorResponse>>> searchDoctors (@PathVariable String departmentName, @PathVariable String specializationName) {
        List<DoctorResponse> doctors = appointmentService.getDoctorsByDepartmentAndSpecialization(departmentName,specializationName);
        return appResponseBuilder.success(HttpStatus.OK,"Doctors found", doctors);
+    }
+    @PostMapping("/patients/{patientId}/doctors/{doctorName}")
+    public ResponseEntity<ResponseStructure<AppointmentResponse>> bookAppointments(@PathVariable Integer patientId, @PathVariable String doctorName, @RequestBody AppointmentRequest appointmentRequest){
+        AppointmentResponse appointmentResponse = appointmentService.bookAppointment(patientId,doctorName,appointmentRequest);
+        return appResponseBuilder.success(HttpStatus.CREATED,"Appointment Booked By Patient",appointmentResponse);
     }
 }
